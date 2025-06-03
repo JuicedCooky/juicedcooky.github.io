331c9b6acf58dc1f3cc79f5c67a0def2fce4e7d3
AlanZh21 <alan.zheng@ontariotechu.net>
2025-06-03 03:38:06 -0400

    Added dotted section scroll bar functionality

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("test");
+
+const sections = document.querySelectorAll("section");
+const dots = document.querySelectorAll(".dot");
+
+const options = {
+    threshold: 0.5,
+};
+
+const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            sectionNumber = entry.target.id[entry.target.id.length-1];
+            dots.forEach(dot => {
+                dot.classList.remove("active");
+                if(sectionNumber == dot.id[dot.id.length-1]){
+                    dot.classList.add("active");
+                }
+            });
+        }
+    });
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
