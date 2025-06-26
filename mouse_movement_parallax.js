2fafd4b678a8357d9c198b4b26824eddf610e659
AlanZh21 <alan.zheng@ontariotechu.net>
2025-06-26 01:05:25 -0400

    modification

--- mouse_movement_parallax.js
+++ mouse_movement_parallax.js
@@ -10,12 +10,12 @@ console.log(`OX:${origin_x}, OY:${origin_y}`);
 let background = document.getElementById("main");
 
 
-document.addEventListener("mousemove", function (event){
-    let time = Date.now();
-    if(time-last_time > 100){
-        console.log("Mouse X:", event.clientX, "Mouse Y:", event.clientY);
-        background.style.backgroundPosition = `${(origin_x-event.clientX)*0.05}px ${(origin_y-event.clientY)*0.05}px`;
-        console.log(background.style.backgroundPosition);
-        last_time = time;
-    }
-});
+//document.addEventListener("mousemove", function (event){
+//    let time = Date.now();
+//    if(time-last_time > 100){
+//        console.log("Mouse X:", event.clientX, "Mouse Y:", event.clientY);
+//        background.style.backgroundPosition = `${(origin_x-event.clientX)*0.05}px ${(origin_y-event.clientY)*0.05}px`;
+//        console.log(background.style.backgroundPosition);
+//        last_time = time;
+//    }
+//});
