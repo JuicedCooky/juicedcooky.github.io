3394f902e9e983638efd6ea70d82f13171869fad
AlanZh21 <alan.zheng@ontariotechu.net>
2025-07-26 03:14:17 -0400

    added links, added text to scroll bar, implemented functionality for contact form

--- js/submit_form.js
+++ js/submit_form.js
@@ -1,3 +1,30 @@
-function submitButton() {
+emailjs.init("uX3-WsfeCz8O7CFaM");
 
+function submitButton() {
+    document.getElementById("submit-button").addEventListener("click", function() {
+
+        const name = document.getElementById("name-text-form").value.trim();
+        const email = document.getElementById("email-text-form").value.trim();
+        //const email = "alanzheng2004@outlook.com";
+        const message = document.getElementById("contact-text-form").value.trim();
+
+        if (!name || !email || !message){
+            alert("Error: Fill all fields.");
+        }
+        else{
+            emailjs.send("service_w3in17p", "template_dwmu37z",
+                          {
+                user_name: name,
+                user_email: "alanzheng2004@outlook.com",
+                message: "Email: " + email + "\n" + message,
+            },
+                          "uX3-WsfeCz8O7CFaM").then(() => {
+                alert("Sent message.");
+            }, (error) => {
+                alert("Failed to send message:", error);
+            });
+        }
+    });
 }
+
+submitButton();
